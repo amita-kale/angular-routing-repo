e0f0cb8c4888681c2db600db80a7a2cd0192adac
Amita kale <amitakale297@gmail.com>
2021-03-19 16:48:42 +0530

    module to module base routing is done

--- src/app/amita/amita-routing.module.ts
+++ src/app/amita/amita-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbcComponent } from './abc/abc.component';
+import { DefComponent } from './def/def.component';
+import { RamComponent } from './ram/ram.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'abc',
+    component: AbcComponent
+  },
+  {
+    path: 'def',
+    component: DefComponent
+  },
+  {
+    path: 'ram',
+    component: RamComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'amita',
+    loadChildren: () => import('./amita/amita.module').then(m => m.AmitaModule)
+  },
+  {
+    path: 'aniket',
+    loadChildren: () => import('./aniket/aniket.module').then(m => m.AniketModule)
+  },
+  {
+    path: 'ashvini',
+    loadChildren: () => import('./ashvini/ashvini.module').then(m => m.AshviniModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
